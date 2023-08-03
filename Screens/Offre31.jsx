53c19ccb2b64c6e4583d736f7dd79bb3e3442152
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-08-03 15:16:05 +0200

    :construction: add offre31 before check stock ok

--- Screens/Offre31.jsx
+++ Screens/Offre31.jsx
@@ -4,21 +4,31 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 import { fonts, colors} from '../styles/styles'
 import {Toast} from 'react-native-toast-message/lib/src/Toast';
 import { Button, RadioButton} from 'react-native-paper'
-import { addToCart} from '../reducers/cartSlice';
+import { addToCart, addFreeProductToCart} from '../reducers/cartSlice';
 import { useSelector, useDispatch } from 'react-redux'
 import { style } from '../styles/formules'; 
 import { styles } from '../styles/home'; 
 import axios from 'axios'
+import { getFamilyProductDetails } from '../CallApi/api';
+import FooterProfile from '../components/FooterProfile';
+import ModalePageOffre31 from '../components/ModalePageOffre';
+
 
 const Offre31 = ({navigation}) => {
 
   const baseUrl = 'http://127.0.0.1:8080';
 
-  const [offre31ProductNames, setoffre31ProductNames] = useState([])
+  const [offre31Products, setOffre31ProductNames] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [familyProductDetails, setFamilyProductDetails] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const familyProductIds = [12, 14]; 
 
     const dispatch = useDispatch()
     const cart = useSelector((state) => state.cart.cart);
-    //console.log('cart', cart)
+    console.log('cart', cart)
 
     const handleBack = () => {
         navigation.navigate('home')
@@ -26,39 +36,18 @@ const Offre31 = ({navigation}) => {
 
       useEffect(() => {
         //les produits ayant une offre 3+1
-        const fetchData = async () => {
+          const fetchData = async () => {
             try {
             const response = await axios.get('http://127.0.0.1:8080/getAllProducts');
           
             const updatedProducts = response.data.map((product) => ({
               ...product,
               qty: 0, 
-
             }));
-            console.log('upd', updatedProducts)
-          //produits offre 3+1
-          const productsOffre = updatedProducts.filter(product => product.offre && product.offre.startsWith("offre31_"))
-          const productsOffreNames = productsOffre.map(product => product.libelle)
-          console.log(productsOffreNames)
 
-          const sortedProducts = productsOffre.sort((a, b) =>
-          a.id_famille_produit - (b.id_famille_produit)
-        );
+            const productsOffre = updatedProducts.filter(product => product.offre && product.offre.startsWith("offre31_"));
     
-        const productsByFamily = {};
-        sortedProducts.forEach((product) => {
-          const { id_famille_produit, libelle } = product;
-          if (!productsByFamily[id_famille_produit]) {
-            productsByFamily[id_famille_produit] = {
-              title: id_famille_produit,
-              products: [],
-            };
-          }
-          productsByFamily[id_famille_produit].products.push({ libelle });
-        });
-
-        console.log('productsByFamily', productsByFamily);
-          setoffre31ProductNames(productsOffreNames)
+            setOffre31ProductNames(productsOffre);
 
           } catch (error) {
             console.error('Une erreur s\'est produite lors de la récupération des produits:', error);
@@ -67,7 +56,72 @@ const Offre31 = ({navigation}) => {
     
         fetchData();
     }, [])
+
+    useEffect(() => {
+      const fetchData = async () => {
+          try {
+            const responses = await Promise.all(
+              familyProductIds.map((id) => getFamilyProductDetails(id))
+            );
+
+            const familleProductDetailsMap = {};
+            responses.forEach((famille) => {
+              if (famille) {
+                familleProductDetailsMap[famille.id] =
+                  famille.name;
+              }
+            });
+            setFamilyProductDetails(familleProductDetailsMap);
+          } catch (error) {
+            console.error(
+              "Une erreur s'est produite lors de la récupération des familles de produits:",
+              error
+            );
+          }
+      };
+  
+      fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (selectedProduct) {
+      setTotalPrice(selectedProduct.prix_unitaire * 3);  // Multiplié par 3 car vous achetez 3 produits
+    }
+  }, [selectedProduct]);
+
+  const handleProduct = (product) => {
+    setSelectedProduct(product)
+    console.log(product)
+}
       
+const handleAcceptOffer = () => {
+  console.log('modale')
+  // Ajoutez le produit trois fois
+  for (let i = 0; i < 3; i++) {
+    dispatch(addToCart({ 
+      productId: selectedProduct.productId, 
+      libelle: selectedProduct.libelle, 
+      image: selectedProduct.image, 
+      prix_unitaire: selectedProduct.prix_unitaire, 
+      qty: 1 , 
+      offre: selectedProduct.offre
+    }));
+  }
+  //le produit gratuit
+  dispatch(addFreeProductToCart(selectedProduct));
+};
+
+const handleCart = () => {
+  setModalVisible(true);
+  // if (selectedProduct) {
+  //     dispatch(addToCart(selectedProduct));
+  //     Toast.show({
+  //       type: 'success',
+  //       position: 'bottom',
+  //       text1: 'Produit ajouté au panier',
+  //     });
+  // }
+}
   return (
     <View style={{flex:1}}>
       <ScrollView>
@@ -89,23 +143,51 @@ const Offre31 = ({navigation}) => {
             <Text style={styles.texteOffre}>Pour l'achat de 3 produits de cette catégorie, vous aurez droit à 1 produit du même type gratuit</Text>
         </View>
         {/* choix produits*/}
-        {Object.values(offre31ProductNames).map((family) => (
-          <View key={family.title}>
-            <Text>{family.title}</Text>
-            <ScrollView horizontal>
-            {family.products && family.products.length > 0 ? (
-              family.products.map((product) => (
-                <View key={product.libelle} style={{ padding: 10 }}>
-                  <Text>{product.libelle}</Text>
-                  {/* ... (Display other information about the product if needed) */}
+        <ScrollView>
+        <View style={{ gap: 20 }}>
+          {Object.values(
+            offre31Products.reduce((groups, product) => {
+              const { id_famille_produit } = product;
+              if (!groups[id_famille_produit]) {
+                groups[id_famille_produit] = {
+                  id_famille_produit: id_famille_produit,
+                  products: [],
+                };
+              }
+              groups[id_famille_produit].products.push(product);
+              return groups;
+            }, {})
+          ).map((group) => (
+            <View key={group.id_famille_produit}>
+              <Text style={{marginLeft:30, marginVertical:10}}>{familyProductDetails[group.id_famille_produit]}</Text>
+              <ScrollView >
+              <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent:'center' }}>
+                {group.products.map((product) => (
+                  <View key={product.libelle} style={{ gap: 10, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: 10 }}>
+                    <Image
+                      source={{ uri: `${baseUrl}/${product.image}` }}
+                      style={style.sandwichImage}
+                    />
+                    <Text>{product.libelle}</Text>
+                    <TouchableOpacity
+                  style={[
+                    style.checkButton,
+                    selectedProduct?.productId === product.productId && { backgroundColor: 'white' } 
+                  ]}
+                  onPress={() => handleProduct(product)}
+                >
+                  {selectedProduct?.productId === product.productId && <View style={style.checkInnerCircle} />}
+                </TouchableOpacity>
+
                 </View>
-              ))
-            ) : (
-              <Text>No products in this family</Text>
-            )}
-            </ScrollView>
-          </View>
-        ))}
+                
+                ))}
+                </View>
+              </ScrollView>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
         
         
     </ScrollView>
@@ -113,24 +195,48 @@ const Offre31 = ({navigation}) => {
     <View style={style.menu}>
         <View>
           <View style={style.bandeauFormule}>
-          <Text style={{ fontWeight:'bold'}}>Prix de la formule</Text>
-         <Text> €</Text>
+          <Text style={{ fontWeight:'bold'}}>Prix de l'offre</Text>
+         <Text> {totalPrice.toFixed(2)}€</Text>
           </View>
           <View style={style.bandeauFormule}>
             <View style={{flexDirection:'row'}}>
             <Text>Avec</Text><Image source={require('../assets/SUN.png')} style={{ width: 50, height: 20, resizeMode:'contain' }}/>
             </View>
-         <Text style={{color:colors.color2, fontWeight:'bold'}}> €</Text>
+         <Text style={{color:colors.color2, fontWeight:'bold'}}> {(totalPrice*0.8).toFixed(2)}€</Text>
           </View>
         </View>
       <Button
                 style={style.btn}
                 textColor={'white'} 
                 // disabled={!selectedSandwich}
+                onPress={handleCart}
                 >Choisir cette offre</Button>
     </View>
+    <FooterProfile />
+    <ModalePageOffre31 modalVisible={modalVisible} setModalVisible={setModalVisible} handleAcceptOffer={handleAcceptOffer} selectedProduct={selectedProduct}/>
+
     </View> 
   )
 }
-
+const styling = StyleSheet.create({
+ 
+  qtyContainer:{
+    flexDirection:'row',
+    gap:5
+  },
+  decrement:{
+    backgroundColor:colors.color3,
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  qtyText:{
+    backgroundColor:colors.color3,
+    paddingHorizontal: 5
+  },
+  increment:{
+    backgroundColor:colors.color2,
+    flexDirection:'row',
+    alignItems:'center'
+  },
+}); 
 export default Offre31
