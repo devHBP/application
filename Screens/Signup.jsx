3917bdceb6b32dc3a13f724951719b6dee805180
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-08-22 11:48:44 +0200

    :white_check_mark: update style  ok

--- Screens/Signup.jsx
+++ Screens/Signup.jsx
@@ -7,7 +7,9 @@ import { registerUser } from '../reducers/authSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import {Toast} from 'react-native-toast-message/lib/src/Toast';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { validateLastName, validateFirstName, validateEmail, validatePassword} from '../validation/validationInput'
+import DatePicker from 'react-native-date-picker';
+import { validateLastName, validateFirstName, validateEmail, validatePassword, validatePostalCode, validateGenre, validateDateOfBirth, validateIdSun} from '../validation/validationInput'
+import ArrowLeft from '../SVG/ArrowLeft'
 
 const inputOptions = {
     style:inputStyling,
@@ -33,8 +35,14 @@ const Signup = ({navigation}) => {
   const [firstname, setFirstName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [cp, setCodePostal] = useState('')
+  const [genre, setGenre] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false)
+  const [formattedDate, setFormattedDate] = useState('');
+  const [idSUN, setIdSun] = useState('')
 
-  const [error, setError] = useState({lastname: '', firstname: '', email: '', password: ''});
+  const [error, setError] = useState({lastname: '', firstname: '', email: '', password: '', cp:'', genre:'', date:'', idSUN:''});
 
   const  submitHandler = () => {
 
@@ -45,20 +53,20 @@ const Signup = ({navigation}) => {
       password,
       //modif ici id_magasin : null au lieu de '' (vide)
       storeId: selectedStore ? selectedStore.storeId : null,
+      cp: parseInt(cp, 10),
+      genre,
+      date_naissance: date.toISOString(),
+      idSUN
     }
     //appel axios post pour s'enregister
     axios.post(`${API_BASE_URL}/signup`, clientData)
     .then(response => {
-      console.log('client data', clientData)
+      //console.log('client data', clientData)
        //console.log('response.data', response.data)
-      
-     
       const userId = response.data.id
-      const user = { userId:userId ,firstname, lastname, email, password}; // Récupérez les données d'inscription du formulaire
+      const user = { userId:userId ,firstname, lastname, email, password, cp, genre,date: date.toISOString(), idSUN}; 
        
-      //console.log('user avec id', user)
-      dispatch(registerUser(user)); // Dispatchez l'action pour mettre à jour l'utilisateur dans le store
-      //  console.log('user dans signup', user)
+      dispatch(registerUser(user)); 
       
        navigation.navigate('stores')
        return Toast.show({
@@ -70,7 +78,7 @@ const Signup = ({navigation}) => {
     .catch(function (error) {
     //console.log('erreur signup',error);
     if (error.response && error.response.status === 400) {
-      //console.log(error.response.data.error);
+      console.log(error.response.data.error);
       Toast.show({
         type: 'error',
         text1: `Erreur d'inscription`,
@@ -79,62 +87,33 @@ const Signup = ({navigation}) => {
     }
     //console.log(clientData)
     });
-   
-    //console.log('test')
   }
+
   const handleBack = () => {
-    navigation.navigate('app');
-  };
+    navigation.navigate('login')
+  }
+ 
 
   return (
     <View style={style.container}>
 
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
       <View >
-        <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
-            <View>
-              <Text style={style.title}>Incription</Text>
-              <Text style={style.pain}>Le pain du jour</Text>
-            </View>
-          <TouchableOpacity onPress={handleBack} style={style.back}>
-           <Icon name="arrow-back-ios" size={20} color="#fff" style={{marginLeft:10}}/>
-         </TouchableOpacity>
+        <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', marginBottom:30}}>
+           
+              <Text style={style.title}>Création de compte</Text>
+              <TouchableOpacity onPress={handleBack} activeOpacity={1} style={{ backgroundColor:'black', borderRadius:25}}>
+                    <ArrowLeft fill="white"/>
+                  </TouchableOpacity>
+       
         </View>
-      
-      <Text style={style.label}>Nom</Text>
-      <TextInput 
-       {...inputOptions}
-        // placeholder='Nom'
-        keyboardType='default'
-        value={lastname}
-        // onChangeText={setLastName}
-        onChangeText={(value) => {
-          setLastName(value);
-          setError({...error, lastname: validateLastName(value)});
-        }}
-        style={error.lastname ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
-      />
-      {error.lastname ? <Text style={{color: 'red', textAlign:'center'}}>{error.lastname}</Text> : null}
 
-      <Text style={style.label}>Prénom</Text>
+      <View style={{paddingBottom:30}}>
+        {/* Email */}
+      <Text style={style.label}>Votre mail</Text>
       <TextInput 
         {...inputOptions}
-        // placeholder='Prénom'
-        keyboardType='default'
-        value={firstname}
-        // onChangeText={setFirstName}
-        onChangeText={(value) => {
-          setFirstName(value);
-          setError({...error, firstname: validateFirstName(value)});
-        }}
-        style={error.firstname ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
-      />
-      {error.firstname ? <Text style={{color: 'red', textAlign:'center'}}>{error.firstname}</Text> : null}
-
-      <Text style={style.label}>Adresse e-mail</Text>
-      <TextInput 
-        {...inputOptions}
-        // placeholder='Email'
+        placeholder='exemple.mail@email.com'
         keyboardType='email-address'
         value={email}
         // onChangeText={setEmail}
@@ -144,12 +123,13 @@ const Signup = ({navigation}) => {
         }}
         style={error.email ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
       />
-       {error.email ? <Text style={{color: 'red', textAlign:'center'}}>{error.email}</Text> : null}
+       {/* {error.email ? <Text style={{color: 'red', textAlign:'center'}}>{error.email}</Text> : null} */}
     
-      <Text style={style.label}>Mot de passe</Text>
+    {/* Mot de passe */}
+      <Text style={style.label}>Votre mot de passe</Text>
       <TextInput 
         {...inputOptions}
-        // placeholder='Mot de passe'
+        placeholder='Mot de passe'
         secureTextEntry={true}
         value={password}
         // onChangeText={setPassword}
@@ -159,27 +139,165 @@ const Signup = ({navigation}) => {
         }}
         style={error.password ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
       />
-      {error.password ? <Text style={{color: 'red', textAlign:'center'}}>{error.password}</Text> : null}
+      {/* {error.password ? <Text style={{color: 'red', textAlign:'center'}}>{error.password}</Text> : null} */}
+      <Text style={style.reglePwd}>Le mot de passe doit inclure au moins 8 caractères, avec au moins 1 lettre majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial</Text>
+      </View>
+      
 
-      <Button
-                style={style.btn} 
-                textColor={'white'} 
-                disabled={lastname === "" || firstname === ""||  email === "" ||  password === "" }
-                onPress={() => {
-                   submitHandler()
-                }}
-                >
-            S'INSCRIRE
-            </Button>
+      <View style={{marginBottom:30}}>
+      <Text style={style.label}>Votre information personnelle</Text>
 
-            <Text style ={{textAlign:'center'}}>Déja un compte ?</Text>
-            <TouchableOpacity onPress={() => navigation.navigate('login')}>
-                <Text style={style.login}>Se Connecter</Text>
-            </TouchableOpacity>
-            </View>
+      {/* genre */}
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap:5, marginVertical:10, justifyContent:'space-around' }}>
+      <Text style={style.texteGenre}>Vous êtes :</Text>
+      <TouchableOpacity 
+          style={style.radioButtonOut}
+          onPress={() => { setGenre('femme'); }}>
+          {
+            genre === 'femme' &&
+            <View style={style.radioButtonIn} />
+          }
+        </TouchableOpacity>
+        <Text style={style.texteGenre}>Mme.</Text>
+        <TouchableOpacity 
+          style={style.radioButtonOut}
+          onPress={() => { setGenre('homme'); }}>
+          {
+            genre === 'homme' &&
+            <View style={style.radioButtonIn} />
+          }
+        </TouchableOpacity>
+        <Text style={style.texteGenre}>M.</Text>
+        <TouchableOpacity 
+          style={style.radioButtonOut}
+          onPress={() => { setGenre('nbinaire'); }}>
+          {
+            genre === 'nbinaire' &&
+            <View style={style.radioButtonIn} />
+          }
+        </TouchableOpacity>
+        <Text style={style.texteGenre}>Non-binaire</Text>
+      </View>
 
-            </ScrollView> 
+      {/* Nom */}
+      {/* <Text style={style.label}>Nom</Text> */}
+      <TextInput 
+       {...inputOptions}
+        placeholder='Nom'
+        keyboardType='default'
+        value={lastname}
+        // onChangeText={setLastName}
+        onChangeText={(value) => {
+          setLastName(value);
+          setError({...error, lastname: validateLastName(value)});
+        }}
+        style={error.lastname ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
+      />
+      {/* {error.lastname ? <Text style={{color: 'red', textAlign:'center'}}>{error.lastname}</Text> : null} */}
+
+      {/* Prenom */}
+      {/* <Text style={style.label}>Prénom</Text> */}
+      <TextInput 
+        {...inputOptions}
+        placeholder='Prénom'
+        keyboardType='default'
+        value={firstname}
+        // onChangeText={setFirstName}
+        onChangeText={(value) => {
+          setFirstName(value);
+          setError({...error, firstname: validateFirstName(value)});
+        }}
+        style={error.firstname ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
+      />
+      {/* {error.firstname ? <Text style={{color: 'red', textAlign:'center'}}>{error.firstname}</Text> : null} */}
+
+        {/* code postal */}
+        {/* <Text style={style.label}>Code postal</Text> */}
+        <TextInput 
+        {...inputOptions}
+        placeholder='Code postal'
+        keyboardType='numeric'
+        value={cp}
+        // onChangeText={setPassword}
+        onChangeText={(value) => {
+          setCodePostal(value);
+          setError({...error, cp: validatePostalCode(value)});
+        }}
+        style={error.cp ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
+      />
+      {/* {error.cp ? <Text style={{color: 'red', textAlign:'center'}}>{error.cp}</Text> : null} */}
+
+      {/* date de naissance */}
+      {/* <Text style={style.label}>Date de naissance</Text> */}
+      <TextInput 
+          {...inputOptions}
+          value={formattedDate}
+          editable={false}  // Empêche l'utilisateur de taper manuellement dans ce champ
+          placeholder="Date de naissance"
+          onTouchStart={() => setOpen(true)}  // Ouvre le date picker lorsque le champ est touché
+      />
+      <DatePicker
+          modal
+          mode='date'
+          minimumDate={new Date(1900, 0, 1)}  // 1 Janvier 1900
+          maximumDate={new Date()}
+          open={open}
+          date={date}
+          onConfirm={(date) => {
+            setOpen(false);
+            setDate(date);
+            const formatted = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+            setFormattedDate(formatted);
+            setError({...error, date: validateDateOfBirth(date)});
+        }}        
+          onCancel={() => {
+              setOpen(false);
+          }}
+      />
+      {/* {error.date ? <Text style={{color: 'red', textAlign:'center'}}>{error.date}</Text> : null} */}
+
+      </View>
+
+      {/* numero client SUN */}
+      <View>
+        <Text style={style.label}>Êtes vous client SUN</Text>
+        <TextInput 
+          {...inputOptions}
+          placeholder='Votre numéro de compte SUN'
+          keyboardType='numeric'
+          value={idSUN}
+          onChangeText={(value) => {
+            setIdSun(value);
+            setError({...error, idSUN: validateIdSun(value)});
+
+          }}
+          style={inputOptions.style}
+        />
+      </View>
+      
+    <View style={{flexDirection:'row', justifyContent:'flex-end'}}>
+    <Button
+        style={style.btn} 
+        textColor={'white'} 
+        disabled={lastname === "" || firstname === ""||  email === "" ||  password === ""  }
+        onPress={() => {
+          submitHandler()
+        }}
+        >
+      Suivant
+      </Button>
     </View>
+      
+        <View style={{marginBottom:50}}>
+          <TouchableOpacity onPress={() => navigation.navigate('login')}>
+            <Text style={style.login}>Vous avez déja un compte ?</Text>
+          </TouchableOpacity>
+        </View>
+      
+    </View>
+
+     </ScrollView> 
+</View>
   )
 }
 
@@ -191,7 +309,7 @@ const style = StyleSheet.create({
         justifyContent:'center',
         //reajustement margin pour laisser de la place au footer
         // marginBottom:70,
-        backgroundColor:colors.color3
+        backgroundColor:colors.color1
     },
   inputOpts : {
     height:50,
@@ -200,11 +318,10 @@ const style = StyleSheet.create({
   },
   btn: {
     backgroundColor: colors.color2,
-    margin: 5,
-    padding: 6,
+    paddingHorizontal: 10,
+    paddingVertical:0,
     borderRadius:6,
-    marginHorizontal:40,
-    marginTop:40
+    marginVertical:10,
   },
   back:{
     backgroundColor: colors.color1,
@@ -216,25 +333,48 @@ const style = StyleSheet.create({
   },
   title:{
     marginVertical:5,
-    color:colors.color2,
-    fontSize:33,
+    color:colors.color6,
+    fontSize:24,
     fontWeight: "900",
-    fontFamily:fonts.font1
-  },
-  pain:{
-    fontStyle:'italic',
-    fontSize:22,
+    fontFamily:fonts.font1,
+    width:"60%"
   },
   login:{
     textAlign:'center',
-    color:colors.color2,
-    fontWeight:"bold",
-    marginVertical:10
+    color:colors.color6,
+    marginVertical:10,
+    textDecorationLine:'underline'
   },
   label:{
     // marginLeft:20,
-   marginTop:10,
-    color:colors.color2
+    marginTop:10,
+    color:colors.color2,
+    fontWeight:fonts.font2,
+    fontWeight:"700"
+  },
+  radioButtonOut:{
+    height: 15,
+    width: 15,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor:colors.color6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:colors.color6
+  },
+  radioButtonIn:{
+    height: 10,
+    width: 10,
+    borderRadius: 6,
+    backgroundColor: colors.color1,
+  },
+  reglePwd:{
+    color: colors.color3,
+    fontSize:10,
+    marginVertical:5
+  },
+  texteGenre:{
+    color:colors.color6
   }
 })
 
