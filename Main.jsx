dc1b8cb07d0555e85332a9c6b66dce4c6cf4ccf3
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-08-23 16:37:50 +0200

    :recycle: refacto

--- Main.jsx
+++ Main.jsx
@@ -42,6 +42,7 @@ import PageQuiche from './Screens/PagesSalees/PageQuiche'
 import PageCroque from './Screens/PagesSalees/PageCroque'
 import Pwd from './Screens/Pwd'
 import LoaderHome from './Screens/LoaderHome'
+import TestSticky from './Screens/TestSticky'
 
 
 const Main = () => {
@@ -69,6 +70,8 @@ const Main = () => {
                 <Stack.Screen name="stores" component={Stores}/>
                 <Stack.Screen name='home' component={Home}/>
                 <Stack.Screen name='loaderhome' component={LoaderHome}/>
+                {/*  test sticky */}
+                <Stack.Screen name='test' component={TestSticky}/>
                     {/*  Formules  */}
                     <Stack.Screen name='formulesandwich' component={FormuleSandwich}/>
                     <Stack.Screen name='formulepoke' component={FormulePoke}/>
