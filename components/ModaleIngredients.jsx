c346968a17544b6c13c47a27740a122934a2c8a2
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-09-13 10:35:21 +0200

    :white_check_mark: update modal ingredients

---
+++ components/ModaleIngredients.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, Modal, StyleSheet, TouchableOpacity } from 'react-native';
+import { fonts, colors} from '../styles/styles'
+
+const ModaleIngredients = ({ modalVisibleIngredients, setModalVisibleIngredients, product }) => {
+
+    const onClose = () => {
+        setModalVisibleIngredients(!modalVisibleIngredients);
+    }
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisibleIngredients}
+      onRequestClose={() => {
+        setModalVisibleIngredients(!modalVisibleIngredients);
+      }}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+        <TouchableOpacity onPress={onClose} style={{ alignItems: 'center', position:'absolute', top:0, right:10}}>
+                <Text style={{ fontSize: 36}}>&times;</Text>
+        </TouchableOpacity>
+         <View style={{flexDirection:'colum', alignItems:'center', gap:10}}>
+            <Text style={styles.title}>Liste d'ingrédients</Text>
+            <Text style={styles.liste}>{product}</Text>
+         </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  modalContent: {
+    backgroundColor: colors.color4,
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    height: 180,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title:{
+    color:colors.color2,
+    fontFamily:fonts.font2,
+    fontSize:18,
+    fontWeight:"700"
+  },
+  liste:{
+    color:colors.color1,
+    textTransform: 'capitalize'
+  }
+});
+
+export default ModaleIngredients;
