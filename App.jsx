b76c5aa730e7a0645183e529c9ef48bb0f8980a4
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-12-08 10:16:00 +0100

    :wrench: update configuration notif

--- App.jsx
+++ App.jsx
@@ -1,51 +1,20 @@
-import React, {useEffect} from 'react';
-import {Alert} from 'react-native';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
 import Main from './Main';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
-import messaging from '@react-native-firebase/messaging';
+import FCMManager from './components/FCMManager';
 
 const App = () => {
 
-
-  useEffect(() => {
-     if (requestUserPermission()){
-      messaging().getToken().then( token => {
-        console.log('Token:', token)
-      })
-     }
-     else {
-      console.log('Failed token status', authStatus)
-     }
-
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      // Accéder directement à la propriété `body` de l'objet `notification`
-      const messageBody = remoteMessage.notification.body;
-      Alert.alert(
-        'Le Pain du Jour',
-        messageBody || 'You received a new message',
-      );
-    });
-
-    return unsubscribe;
-  }, []);
+  
 
   return (
     <Provider store={store}>
+      <FCMManager />
       <Main />
     </Provider>
   );
 };
-
-async function requestUserPermission() {
-  const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  if (enabled) {
-    console.log('Authorization status:', authStatus);
-  }
-}
 
 export default App;
---
+++ components/FCMManager.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { API_BASE_URL } from '@env';
+
+const FCMManager = () => {
+    const user = useSelector((state) => state.auth.user);
+    const [tokenInitialized, setTokenInitialized] = useState(false);
+
+    const sendTokenToBackend = async (fcmToken) => {
+        try {
+            const response = await axios.post(`${API_BASE_URL}/saveToken`, {
+                userId: user.userId, 
+                fcmToken: fcmToken,
+            });
+            console.log('Token envoyé au backend avec succès', response.data);
+        } catch (error) {
+            console.error("Erreur lors de l'envoi du token au backend", error);
+        }
+    };
+
+    useEffect(() => {
+        if (!tokenInitialized && user && user.userId) {
+            const initializeFCM = async () => {
+                const authStatus = await messaging().requestPermission();
+                const enabled =
+                    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+                    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+                if (enabled) {
+                    console.log('Authorization status:', authStatus);
+                    const token = await messaging().getToken();
+                    console.log('FCM Token dans composant FCM:', token);
+                    if (token) {
+                        sendTokenToBackend(token);
+                    }
+                } else {
+                    console.log('Failed token status', authStatus);
+                }
+            };
+
+            initializeFCM();
+            setTokenInitialized(true);
+        }
+
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            const messageBody = remoteMessage.notification.body;
+            Alert.alert(
+                'Le Pain du Jour',
+                messageBody || 'You received a new message',
+            );
+        });
+
+        return unsubscribe;
+    }, [user, tokenInitialized]);
+
+    return null;
+};
+
+export default FCMManager;
