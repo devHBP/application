80e9dbdc82c7e94def38d13ec752d34614033be9
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-02-08 09:11:09 +0100

    :wrench: update page de maintenance

--- App.jsx
+++ App.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Main from './Main';
 import {Provider} from 'react-redux';
 import store from './store';
 import FCMManager from './components/FCMManager';
 import { CountdownProvider } from './components/CountdownContext';  
+import axios from 'axios'; 
+import {API_BASE_URL} from './config';
+import Maintenance from './Screens/Maintenance';
 
 
 const App = () => {
+
+  const [serverStatus, setServerStatus] = useState(false);
+ 
+
+  useEffect(() => {
+    axios.get(`${API_BASE_URL}/status`)
+      .then(response => {
+        setServerStatus(response.data.maintenanceMode);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la vérification du statut du serveur', error);
+        setServerStatus('error');
+      });
+  }, []);
+
+  if (serverStatus === 'maintenance' ) {
+    return <Maintenance />; 
+  }
+
   return (
     <Provider store={store}>
       <CountdownProvider>
--- Screens/Maintenance.jsx
+++ Screens/Maintenance.jsx
@@ -1,24 +1,44 @@
-import { View, Text, StyleSheet, Alert } from 'react-native'
-import React from 'react'
-import { colors, fonts } from '../styles/styles'
-
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { colors } from '../styles/styles';
 
 const Maintenance = () => {
   return (
-    <View style={style.container}>
-      <Text style={style.textMaintenance}>Maintenance</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Maintenance en Cours</Text>
+      <Text style={styles.text}>Nous améliorons actuellement votre expérience sur l'application "Le Pain du Jour Click and Collect".</Text>
+      <Text style={styles.text}>Merci pour votre patience et votre compréhension.</Text>
+      <Text style={styles.footer}>L'équipe du Pain du Jour</Text>
     </View>
   )
 }
-const style = StyleSheet.create({
+
+const styles = StyleSheet.create({
     container: {
         flex: 1, 
         backgroundColor: colors.color1,
         justifyContent: 'center', 
         alignItems: 'center',
+        padding: 20,
     },
-    textMaintenance:{
-        color:colors.color3
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: colors.color3,
+        marginBottom: 15,
+    },
+    text: {
+        color: colors.color3,
+        fontSize: 16,
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    footer: {
+        color: colors.color3,
+        fontSize: 14,
+        marginTop: 20,
+        fontStyle: 'italic',
     }
 });
-export default Maintenance
+
+export default Maintenance;
