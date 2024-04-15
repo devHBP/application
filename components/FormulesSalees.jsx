2389ab09809f512c31dc762a9b34cfaa214c51da
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-04-15 12:16:47 +0200

    :wrench: update app without solanid products

--- components/FormulesSalees.jsx
+++ components/FormulesSalees.jsx
@@ -21,7 +21,6 @@ const FormulesSalees = () => {
 
     const data = [
         { name: 'Sandwich', imageUri: FormuleSandwich, action: () => navigation.navigate('formulesandwich'), description: "Un sandwich, un dessert et une boisson" },
-        { name: 'Plat Chaud', imageUri: FormulePlatsChauds, action: () => navigation.navigate('formuleplatschauds'), description: "Un plat chaud, un dessert et une boisson" },
         { name: 'Pain Bagnat', imageUri: FormulePainBagnat, action: () => navigation.navigate('formulepainbagnat'), description: "Un pain bagnat, un dessert et une boisson" },
         { name: 'Salade', imageUri: FormuleSalade, action: () => navigation.navigate('formulesalade'), description: "Une salade, un dessert et une boisson" },
         { name: 'Burger', imageUri: FormuleBurger, action: () => navigation.navigate('formuleburger'), description: "Un burger, un dessert et une boisson" },
@@ -30,7 +29,9 @@ const FormulesSalees = () => {
         { name: 'Croque', imageUri: FormuleCroque, action: () => navigation.navigate('formulecroque'), description:"Un croque, un dessert et une boisson" },
         { name: 'Poke Bowl', imageUri: PokeSaumon, action: () => navigation.navigate('formulepoke'), description: "Un Poke Bowl, un dessert et une boisson" },
         { name: 'Panini', imageUri: FormulePanini, action: () => navigation.navigate('formulepanini'), description:"Un panini, un dessert et une boisson" },
-        { name: 'Quiche', imageUri: FormuleQuiche, action: () => navigation.navigate('formulequiche'), description:"Une quiche, un dessert et une boisson" },
+        { name: 'Quiche', imageUri: FormuleQuiche, action: () => navigation.navigate('formulequiche'), description:"Une quiche, un dessert et une boisson" }
+
+        // { name: 'Plat Chaud', imageUri: FormulePlatsChauds, action: () => navigation.navigate('formuleplatschauds'), description: "Un plat chaud, un dessert et une boisson" },
         // { name: 'Quiche', imageUri: 'https://cdn.lepaindujour.io/assets/Formule22.jpg', action: () => navigation.navigate('formulequiche'), description:"Une quiche, un dessert et une boisson" },
 
     ];
